1583e610da8b537d002d3fc1c6160b75ce66df87
kamalruidasdcg <145760654+kamalruidasdcg@users.noreply.github.com>
2023-11-27 17:31:49 +0530

    update 1 (#29)
    Co-authored-by: mainakweb <mainak.dutta16@gmail.com>

---
+++ controllers/poController/icgrnController.js
@@ -0,0 +1,26 @@
+const { resSend } = require("../../lib/resSend");
+const { query } = require("../../config/dbConfig");
+const { generateQuery, getEpochTime } = require("../../lib/utils");
+const { INSERT } = require("../../lib/constant");
+const { ICGRN } = require("../../lib/tableName");
+const { SUBMITTED, REJECTED, ACKNOWLEDGED, APPROVED, RE_SUBMITTED, CREATED } = require("../../lib/status");
+const fileDetails = require("../../lib/filePath");
+const path = require('path');
+const { wdcPayload } = require("../../services/po.services");
+const { handleFileDeletion } = require("../../lib/deleteFile");
+const { getFilteredData, updatTableData, insertTableData } = require("../genralControlles");
+
+
+
+exports.List = async (req, res) => {
+    
+    req.query.$tableName = ICGRN;
+    req.query.$filter = `{ "purchasing_doc_no" :  ${req.query.poNo}}`;
+    try {
+      getFilteredData(req, res);
+    } catch(err) {
+      console.log("data not fetched", err);
+    }
+  // resSend(res, true, 200, "oded!", req.query.dd, null);
+   
+}
---
+++ controllers/poController/paymentAdviseController.js
@@ -0,0 +1,28 @@
+const { resSend } = require("../../lib/resSend");
+const { query } = require("../../config/dbConfig");
+const { generateQuery, getEpochTime } = require("../../lib/utils");
+const { INSERT } = require("../../lib/constant");
+const { PAYMENTADVICE } = require("../../lib/tableName");
+const { SUBMITTED, REJECTED, ACKNOWLEDGED, APPROVED, RE_SUBMITTED, CREATED } = require("../../lib/status");
+const fileDetails = require("../../lib/filePath");
+const path = require('path');
+const { wdcPayload } = require("../../services/po.services");
+const { handleFileDeletion } = require("../../lib/deleteFile");
+const { getFilteredData, updatTableData, insertTableData } = require("../genralControlles");
+
+
+
+exports.List = async (req, res) => {
+    
+    req.query.$tableName = PAYMENTADVICE;
+    req.query.$filter = `{ "purchasing_doc_no" :  ${req.query.poNo}}`;
+    //console.log(req.query);
+    //return;
+    try {
+      getFilteredData(req, res);
+    } catch(err) {
+      console.log("data not fetched", err);
+    }
+  // resSend(res, true, 200, "oded!", req.query.dd, null);
+   
+}
