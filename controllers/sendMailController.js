d0ab876b3d06cd60cfb4d0aaec84dada91093802
unknown <kamal.ruidas@datacoresystems.co.in>
2023-12-05 18:04:28 +0530

    drawing and qap also working

--- controllers/sendMailController.js
+++ controllers/sendMailController.js
@@ -1,7 +1,7 @@
 const SENDMAIL = require("../lib/mailSend");
 const mailBody = require("../lib/mailBody");
-const { SDBG_SUBMIT_MAIL_TEMPLATE } = require("../templates/mail-template");
-const { SDBG_SUBMIT_BY_VENDOR, SDBG_SUBMIT_BY_GRSE, SDBG_ACKNOWLEDGE_BY_GRSE } = require("../lib/event");
+const { SDBG_SUBMIT_MAIL_TEMPLATE, DRAWING_SUBMIT_MAIL_TEMPLATE, QAP_SUBMIT_MAIL_TEMPLATE } = require("../templates/mail-template");
+const { SDBG_SUBMIT_BY_VENDOR, SDBG_SUBMIT_BY_GRSE, SDBG_ACKNOWLEDGE_BY_GRSE, DRAWING_SUBMIT_BY_VENDOR, DRAWING_SUBMIT_BY_GRSE, DRAWING_APPROVED_BY_GRSE, QAP_SUBMIT_BY_VENDOR, QAP_SUBMIT_BY_GRSE, QAP_APPROVED_BY_GRSE } = require("../lib/event");
 const { mailInsert, updateMailStatus } = require("../services/mai.services");
 const path = require('path');
 
@@ -62,12 +62,112 @@ const mailTrigger = async (payload = check(), eventName = check()) => {
                 break;
             case SDBG_ACKNOWLEDGE_BY_GRSE:
 
-                mail_body = mailBody["SDBG_VENDOR_MAIL_BODY"].replace(/{{(.*?)}}/g, (match, p1) => payload[p1.trim()] || match);
+                mail_body = mailBody["DRAWING_VENDOR_MAIL_BODY"].replace(/{{(.*?)}}/g, (match, p1) => payload[p1.trim()] || match);
 
                 mailDetails = {
                     to: payload.mailSendTo,
                     subject: "Submission of SDBG",
                     html: SDBG_SUBMIT_MAIL_TEMPLATE(mail_body, "Vendor update"),
+                    // attachments: [{
+                    //     filename: payload.fileName,
+                    //     path: payload.filePath,
+                    // }]
+                };
+
+                break;
+            case DRAWING_SUBMIT_BY_VENDOR:
+                /**
+                 * REPLACE VARIABLES IN MAIL BODY ,  PAYLOAD OBJECT SHOULD ALL THOSE VALUES
+                 * vendor_name purchasing_doc_no action_by_name action_by_id remarks created_at 
+                 * && mailSendTo[MAIL SENDER MAIL ID]
+                 */
+                mail_body = mailBody["DRAWING_GRSE_MAIL_BODY"].replace(/{{(.*?)}}/g, (match, p1) => payload[p1.trim()] || match);
+
+                mailDetails = {
+                    to: payload.mailSendTo,
+                    subject: "Submission of SDBG",
+                    html: DRAWING_SUBMIT_MAIL_TEMPLATE(mail_body, "GRSR updated"),
+                    // attachments: [{
+                    //     filename: payload.fileName,
+                    //     path: payload.filePath,
+                    // }]
+                };
+
+                break;
+            case DRAWING_SUBMIT_BY_GRSE:
+
+                mail_body = mailBody["SDBG_VENDOR_MAIL_BODY"].replace(/{{(.*?)}}/g, (match, p1) => payload[p1.trim()] || match);
+
+                mailDetails = {
+                    to: payload.mailSendTo,
+                    subject: "Submission of SDBG",
+                    html: DRAWING_SUBMIT_MAIL_TEMPLATE(mail_body, "Vendor update"),
+                    // attachments: [{
+                    //     filename: payload.fileName,
+                    //     path: payload.filePath,
+                    // }]
+                };
+
+
+                break;
+            case DRAWING_APPROVED_BY_GRSE:
+
+                mail_body = mailBody["DRAWING_VENDOR_MAIL_BODY"].replace(/{{(.*?)}}/g, (match, p1) => payload[p1.trim()] || match);
+
+                mailDetails = {
+                    to: payload.mailSendTo,
+                    subject: "Submission of Drawing",
+                    html: DRAWING_SUBMIT_MAIL_TEMPLATE(mail_body, "Vendor update"),
+                    // attachments: [{
+                    //     filename: payload.fileName,
+                    //     path: payload.filePath,
+                    // }]
+                };
+
+                break;
+            case QAP_SUBMIT_BY_VENDOR:
+                /**
+                 * REPLACE VARIABLES IN MAIL BODY ,  PAYLOAD OBJECT SHOULD ALL THOSE VALUES
+                 * vendor_name purchasing_doc_no action_by_name action_by_id remarks created_at 
+                 * && mailSendTo[MAIL SENDER MAIL ID]
+                 */
+                mail_body = mailBody["QAP_GRSE_MAIL_BODY"].replace(/{{(.*?)}}/g, (match, p1) => payload[p1.trim()] || match);
+
+                mailDetails = {
+                    to: payload.mailSendTo,
+                    subject: "Submission of QAP",
+                    html: QAP_SUBMIT_MAIL_TEMPLATE(mail_body, "GRSR updated"),
+                    // attachments: [{
+                    //     filename: payload.fileName,
+                    //     path: payload.filePath,
+                    // }]
+                };
+
+                break;
+            case QAP_SUBMIT_BY_GRSE:
+
+                mail_body = mailBody["QAP_VENDOR_MAIL_BODY"].replace(/{{(.*?)}}/g, (match, p1) => payload[p1.trim()] || match);
+
+                mailDetails = {
+                    to: payload.mailSendTo,
+                    subject: "Submission of QAP",
+                    html: QAP_SUBMIT_MAIL_TEMPLATE(mail_body, "Vendor update"),
+                    // attachments: [{
+                    //     filename: payload.fileName,
+                    //     path: payload.filePath,
+                    // }]
+                };
+
+
+                break;
+            case QAP_APPROVED_BY_GRSE:
+
+                mail_body = mailBody["QAP_VENDOR_MAIL_BODY"].replace(/{{(.*?)}}/g, (match, p1) => payload[p1.trim()] || match);
+
+                mailDetails = {
+                    to: payload.mailSendTo,
+                    subject: "Approval of QAP",
+                    html: QAP_SUBMIT_MAIL_TEMPLATE(mail_body, "Vendor update"),
                     // attachments: [{
                     //     filename: payload.fileName,
                     //     path: payload.filePath,
