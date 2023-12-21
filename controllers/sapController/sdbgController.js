45b238ef41587f0698f5fdf34ff15e7692c2bb31
mainakweb <mainak.dutta16@gmail.com>
2023-12-21 10:42:57 +0530

    changes

--- controllers/sapController/sdbgController.js
+++ controllers/sapController/sdbgController.js
@@ -4,12 +4,25 @@ const { zfi_bgm_1_Payload } = require("../../services/sap.services");
 const { SDBG_PAYMENT_ADVICE } = require('../../lib/tableName');
 const { generateQuery, generateQueryArray } = require('../../lib/utils');
 const { query } = require('../../config/dbConfig');
+const mysql = require("mysql2/promise");
+require("dotenv").config();
+
 const { INSERT } = require('../../lib/constant');
 
 exports.sdbgPaymentAdvice = async (req, res) => {
 
 //    http://10.13.1.38:4001/api/v1/po/qap
     try {
+        const connObj = {
+            host: process.env.DB_HOST_ADDRESS,
+            port: process.env.DB_CONN_PORT,
+            user: process.env.DB_USER,
+            password: "",
+            database: process.env.DB_NAME,
+        }
+
+        const promiseConnection = await mysql.createConnection(connObj);
+
         if(!req.body) {
             resSend(res, false, 400, "Please send a valid payload.", null, null);
         }
@@ -21,9 +34,12 @@ exports.sdbgPaymentAdvice = async (req, res) => {
         console.log(q);
         console.log("uukufj");
         console.log(val);
+        let dd = `INSERT INTO zfi_bgm_1 (FILE_NO,BANKERS_NAME) VALUES ?`;
 
        // return;
-        const response = await query({ query: q, values: [val] });
+       // const response = await query({ query: q, values: [val] });
+
+       const response = await promiseConnection.query(dd, [val]);
 
         resSend(res, true, 200, "Data inserted successfully", response, null);
     } catch (err) {
