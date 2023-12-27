42432f1c13cb1d36490191b7ce9fbd1c505dd8bc
unknown <kamal.ruidas@datacoresystems.co.in>
2023-12-27 14:12:37 +0530

    response status change

--- controllers/sapController/sdbgController.js
+++ controllers/sapController/sdbgController.js
@@ -1,11 +1,10 @@
-const path = require('path');
-const { resSend } = require("../../lib/resSend");
+
+const {  responseSend } = require("../../lib/resSend");
 const { zfi_bgm_1_Payload } = require("../../services/sap.services");
 const { SDBG_PAYMENT_ADVICE } = require('../../lib/tableName');
-const { generateQuery, generateQueryArray } = require('../../lib/utils');
+const {  generateQueryArray } = require('../../lib/utils');
 const { connection } = require('../../config/dbConfig');
 // const mysql = require("mysql2/promise");
-require("dotenv").config();
 
 const { INSERT } = require('../../lib/constant');
 
@@ -15,16 +14,16 @@ exports.sdbgPaymentAdvice = async (req, res) => {
     const promiseConnection = await connection();
     try {
         if (!req.body) {
-            resSend(res, false, 400, "Please send a valid payload.", null, null);
+            responseSend(res, "0", 400, "Please send a valid payload.", null, null);
         }
         const payload = { ...req.body };
         const payloadObj = await zfi_bgm_1_Payload(payload);
         const { q, val } = await generateQueryArray(INSERT, SDBG_PAYMENT_ADVICE, payloadObj);
         const response = await promiseConnection.query(q, [val]);
-        resSend(res, true, 200, "Data inserted successfully", response, null);
+        responseSend(res, "1", 200, "Data inserted successfully", response, null);
     } catch (err) {
         console.log("data not fetched", err);
-        resSend(res, false, 500, "Internal server error", null, null);
+        responseSend(res, "0", 500, "Internal server error", null, null);
     } finally {
         await promiseConnection.end();
     }
