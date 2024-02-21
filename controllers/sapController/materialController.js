87cfed09fa833f9b7b48799725e70653d5495e7e
kamalruidasdcg <145760654+kamalruidasdcg@users.noreply.github.com>
2024-02-21 12:32:07 +0530

    mseg table (#95)

--- controllers/sapController/materialController.js
+++ controllers/sapController/materialController.js
@@ -1,17 +1,18 @@
 
-const { NEW_SDBG, MAKT } = require('../../lib/tableName');
+const { NEW_SDBG, MAKT, SDBG_PAYMENT_ADVICE, MSEG } = require('../../lib/tableName');
 const { connection } = require("../../config/dbConfig");
 const { INSERT } = require("../../lib/constant");
 const { responseSend, resSend } = require("../../lib/resSend");
-const { generateQuery } = require("../../lib/utils");
+const { generateQuery, generateQueryArray } = require("../../lib/utils");
 const { getFilteredData } = require('../genralControlles');
+const { msegPayload } = require("../../services/sap.material.services")
 
 
 const makt = async (req, res) => {
     let insertPayload = {};
-    
+
     try {
-     
+
         const promiseConnection = await connection();
         let transactionSuccessful = false;
 
@@ -32,7 +33,7 @@ const makt = async (req, res) => {
                 MAKTG: obj.MAKTG ? obj.MAKTG : null,
             };
 
-            const ekkoTableInsert = generateQuery(INSERT, MAKT , insertPayload);
+            const ekkoTableInsert = generateQuery(INSERT, MAKT, insertPayload);
 
             try {
                 const [results] = await promiseConnection.execute(ekkoTableInsert["q"], ekkoTableInsert["val"]);
@@ -44,7 +45,7 @@ const makt = async (req, res) => {
             transactionSuccessful = true;
 
             return responseSend(res, "1", 200, "data insert succeed.", [], null);
-            
+
         } catch (error) {
             responseSend(res, "0", 502, "Data insert failed !!", error, null);
         }
@@ -59,6 +60,33 @@ const makt = async (req, res) => {
         responseSend(res, "0", 400, "Error in database conn!!", error, null);
     }
 };
+const mseg = async (req, res) => {
+  
+    const promiseConnection = await connection();
+    try {
+        if (!req.body) {
+            responseSend(res, "0", 400, "Please send a valid payload.", null, null);
+        }
+        const payload = req.body;
+        
+        const payloadObj = await msegPayload(payload);
+        const { q, val } = await generateQueryArray(INSERT, MSEG, payloadObj);
+
+        console.log("q", q);
+        console.log("val", val);
+
+        const response = await promiseConnection.query(q, [val]);
+        responseSend(res, "1", 200, "Data inserted successfully", response, null);
+    } catch (err) {
+        console.log("data not inserted", err);
+        responseSend(res, "0", 500, "Internal server errorR", err, null);
+    } finally {
+        await promiseConnection.end();
+    }
+
+
+};
+
 
 
 
@@ -77,4 +105,4 @@ const list = async (req, res) => {
 }
 
 
-module.exports = { list, makt }
+module.exports = { list, makt, mseg }
