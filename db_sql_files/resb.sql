7aab451b19828d7b407d22857a619751e59611e4
unknown <kamal.ruidas@datacoresystems.co.in>
2024-03-08 15:47:42 +0530

    db update

--- db_sql_files/resb.sql
+++ db_sql_files/resb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 06, 2024 at 11:01 AM
+-- Generation Time: Mar 08, 2024 at 11:16 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -31,6 +31,7 @@ CREATE TABLE `resb` (
   `C_PKEY` varchar(20) NOT NULL,
   `RSNUM` int(10) DEFAULT NULL COMMENT 'Number of reservation/dependent requirements',
   `RSPOS` int(4) DEFAULT NULL COMMENT 'Item Number of Reservation / Dependent Requirements',
+  `RSART` varchar(1) NOT NULL COMMENT 'Record type',
   `BDART` varchar(2) DEFAULT NULL COMMENT 'Requirement type',
   `RSSTA` varchar(1) DEFAULT NULL COMMENT 'Status of reservation',
   `KZEAR` varchar(1) DEFAULT NULL COMMENT 'Final Issue for Reservation',
@@ -38,19 +39,16 @@ CREATE TABLE `resb` (
   `WERKS` varchar(4) DEFAULT NULL COMMENT 'Plant',
   `LGORT` varchar(4) DEFAULT NULL COMMENT 'Storage Location',
   `CHARG` varchar(10) DEFAULT NULL COMMENT 'Batch Number',
-  `BDMNG` bigint(13) DEFAULT NULL COMMENT 'Requirement Quantity',
+  `BDMNG` decimal(13,3) DEFAULT NULL COMMENT 'Requirement Quantity',
   `MEINS` varchar(3) DEFAULT NULL COMMENT 'Base Unit of Measure',
-  `ENMNG` bigint(13) DEFAULT NULL COMMENT 'Quantity withdrawn',
-  `BWART` varchar(3) DEFAULT NULL COMMENT 'Movement Type (Inventory Management)'
+  `ENMNG` decimal(13,3) DEFAULT NULL COMMENT 'Quantity withdrawn',
+  `BWART` varchar(3) DEFAULT NULL COMMENT 'Movement Type (Inventory Management)',
+  `ERFMG` decimal(13,3) DEFAULT NULL COMMENT 'Quantity in Unit of Entry',
+  `XWAOK` varchar(1) DEFAULT NULL COMMENT 'Goods Movement for Reservation Allowed',
+  `XLOEK` varchar(1) DEFAULT NULL COMMENT '	Item is Deleted',
+  `PSPEL` int(8) DEFAULT NULL COMMENT 'WBS Element',
+  `BDTER` date DEFAULT NULL COMMENT 'Requirement Date for the Component'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `resb`
---
-
-INSERT INTO `resb` (`C_PKEY`, `RSNUM`, `RSPOS`, `BDART`, `RSSTA`, `KZEAR`, `MATNR`, `WERKS`, `LGORT`, `CHARG`, `BDMNG`, `MEINS`, `ENMNG`, `BWART`) VALUES
-('345678998-1001', 345678998, 1001, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-('345678998-1002', 345678998, 1002, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
