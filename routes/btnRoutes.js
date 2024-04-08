5a6cf0c25f199b14641ce1f9db14f6b98afd59e6
kamalruidasdcg <145760654+kamalruidasdcg@users.noreply.github.com>
2024-04-08 17:18:32 +0530

    update btn 1 (#130)
    * update btn 1
    
    * gate entry filter query added
    
    ---------
    
    Co-authored-by: mainakweb <mainak.dutta16@gmail.com>

--- routes/btnRoutes.js
+++ routes/btnRoutes.js
@@ -5,23 +5,37 @@ const {
   submitBTN,
   fetchBTNByNum,
   getBTNData,
+  submitBTNByDO,
+  fetchBTNByNumForDO,
 } = require("../controllers/btnControllers");
 const { btnmw } = require("../services/btnmw");
+const { veifyAccessToken } = require("../services/jwt.services");
 
 router.get("/", [], (req, res) => {
   fetchAllBTNs(req, res);
 });
-router.get("/getBTNData", [], (req, res) => {
+router.get("/getBTNData", [veifyAccessToken], (req, res) => {
   getBTNData(req, res);
 });
 
-router.get("/btn_num", [], (req, res) => {
+router.get("/btn_num", [veifyAccessToken], (req, res) => {
   fetchBTNByNum(req, res);
 });
 
+router.get("/btn_do", [veifyAccessToken], (req, res) => {
+  fetchBTNByNumForDO(req, res);
+});
+
 const upload = btnmw();
-router.post("/BillsMaterialHybrid", upload, (req, res) => {
+router.post("/BillsMaterialHybrid", [veifyAccessToken, upload], (req, res) => {
   submitBTN(req, res);
 });
+router.post(
+  "/BillsMaterialHybridByDO",
+  [veifyAccessToken, upload],
+  (req, res) => {
+    submitBTNByDO(req, res);
+  }
+);
 
 module.exports = router;
