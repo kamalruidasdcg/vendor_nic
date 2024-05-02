4625664a66a7967f6ca389d98a9f9525585e9e73
mainakweb <mainak.dutta16@gmail.com>
2024-05-02 13:30:01 +0530

    update btns and current chenges

--- controllers/btnControllers.js
+++ controllers/btnControllers.js
@@ -12,7 +12,7 @@ const {
 } = require("../lib/constant");
 const { resSend } = require("../lib/resSend");
 const { APPROVED } = require("../lib/status");
-const { getEpochTime } = require("../lib/utils");
+const { getEpochTime, getYyyyMmDd } = require("../lib/utils");
 const { create_btn_no } = require("../services/po.services");
 const {
   getSDBGApprovedFiles,
@@ -25,6 +25,7 @@ const {
   getBTNInfoDO,
 } = require("../utils/btnUtils");
 const { checkTypeArr } = require("../utils/smallFun");
+
 
 const fetchAllBTNs = async (req, res) => {
   const { id } = req.query;
@@ -77,16 +78,16 @@ const fetchBTNByNum = async (req, res) => {
     query: btnQ,
     values: [id, btn_num],
   });
-  const gate_entry_q = `SELECT ENTRY_NO AS gate_entry_no,
-  ZMBLNR AS grn_no,
-  INV_DATE AS invoice_date FROM zmm_gate_entry_d WHERE EBELN = ? AND INVNO = ?`;
+  // const gate_entry_q = `SELECT ENTRY_NO AS gate_entry_no,
+  // ZMBLNR AS grn_no,
+  // INV_DATE AS invoice_date FROM zmm_gate_entry_d WHERE EBELN = ? AND INVNO = ?`;
   
-  let gate_entry_v = await query({
-    query: gate_entry_q,
-    values: [result[0].purchasing_doc_no, result[0].invoice_no],
-  });
+  // let gate_entry_v = await query({
+  //   query: gate_entry_q,
+  //   values: [result[0].purchasing_doc_no, result[0].invoice_no],
+  // });
 
-  console.log(gate_entry_v);
+  // console.log(gate_entry_v);
   
   return resSend(res, true, 200, "ALL data from BTNs", result, null);
 };
@@ -198,10 +199,10 @@ const getBTNData = async (req, res) => {
     }
 
     // GET GRN Number by PO Number
-    let icgrn_nos = await getICGRNs(id);
-    if (icgrn_nos) {
-      obj = { ...obj, icgrn_nos };
-    }
+    // let icgrn_nos = await getICGRNs(id);
+    // if (icgrn_nos) {
+    //   obj = { ...obj, icgrn_nos };
+    // }
 
     // GET Approved SDBG by PO Number
     let pbg_filename_result = await getPBGApprovedFiles(id);
@@ -313,7 +314,10 @@ const submitBTN = async (req, res) => {
   // let grn_nos = await getGRNs(purchasing_doc_no);
 
   // GET ICGRN Value by PO Number
-  let icgrn_total = await getICGRNs(purchasing_doc_no).total_icgrn_value;
+  let icgrn_total = await getICGRNs({purchasing_doc_no, invoice_no});
+  console.log("icgrn_total", icgrn_total)
+ 
+  icgrn_total=icgrn_total.total_icgrn_value; 
 
   // // GET GRN Number by PO Number
   // let icgrn_nos = await getICGRNs(purchasing_doc_no);
@@ -344,8 +348,9 @@ const submitBTN = async (req, res) => {
   if (!credit_note || credit_note === "") {
     credit_note = 0;
   }
+  
   let net_claim_amount =
-    parseInt(invoice_value) + parseInt(debit_note) - parseInt(credit_note);
+  parseFloat(invoice_value) + parseFloat(debit_note) + parseFloat(gst_rate) - parseFloat(credit_note);
 
   // GET Contractual Dates from other Table
   let c_sdbg_date = null;
@@ -357,7 +362,6 @@ const submitBTN = async (req, res) => {
     query: c_sdbg_date_q,
     values: [purchasing_doc_no],
   });
-  console.log(c_dates);
   const dates_arr = [C_SDBG_DATE, C_DRAWING_DATE, C_QAP_DATE, C_ILMS_DATE];
   for(const item of dates_arr) {
     const i = c_dates.findIndex((el) => el.MTEXT == item);
@@ -424,7 +428,7 @@ const submitBTN = async (req, res) => {
   });
 
   // created at
-  let created_at = new Date().toLocaleDateString();
+  let created_at = getEpochTime(); //new Date().toLocaleDateString();
 
   // INSERT Data into btn table
   let btnQ = `INSERT INTO btn SET 
@@ -477,6 +481,7 @@ const submitBTN = async (req, res) => {
     values: [],
   });
 
+  console.log("result", result)
   if (result.affectedRows) {
     return resSend(
       res,
@@ -531,7 +536,7 @@ const submitBTNByDO = async (req, res) => {
     return resSend(res, false, 200, "BTN is already submitted!", null, null);
   }
   // created at
-  let created_at = new Date().toLocaleDateString();
+  let created_at = new Date().toLocaleDateString(); //getEpochTime(); 
   console.log(created_at);
 
   // INSERT Data into btn table
@@ -556,21 +561,30 @@ const submitBTNByDO = async (req, res) => {
 console.log(result);
 //return;
   // GET BTN Info by BTN Number
-  // let btnInfo = await getBTNInfo(btn_num);
-  // let btnDOInfo = await getBTNInfoDO(btn_num);
-  // console.log("result: " + JSON.stringify(btnInfo));
-  // console.log("btnDOInfo: " + JSON.stringify(btnDOInfo));
+  let btnInfo = await getBTNInfo(btn_num);
+  let btnDOInfo = await getBTNInfoDO(btn_num);
+  console.log("result: " + JSON.stringify(btnInfo));
+  console.log("btnDOInfo: " + JSON.stringify(btnDOInfo));
 
+  const qq = `select t1.LIFNR as vendor_code,t2.NAME1 as vendor_name from ekko as t1 LEFT JOIN 
+  lfa1 as t2 ON t1.LIFNR = t2.LIFNR where t1.EBELN = ?`;
+  let result_qq = await query({
+    query: qq,
+    values: [btnInfo[0].purchasing_doc_no],
+  });
+
+console.log(result_qq);
+console.log("4567898765ji8y");
   const btn_payload = {
     ZBTNO: btnInfo[0]?.btn_num, // BTN Number
     ERDAT: getYyyyMmDd(getEpochTime()), // BTN Create Date
-    ERZET: getYyyyMmDd(getEpochTime()), // BTN Create Time
-    ERNAM: "", // Created Person Name
+    ERZET: timeInHHMMSS(),// 134562,  // BTN Create Time
+    ERNAM: tokenData.vendor_code, // Created Person Name
     LAEDA: "", // Not Needed
-    AENAM: "NAME", // Vendor Name
-    LIFNR: "50000437", // Vendor Codebtn_v2
-    ZVBNO: btnInfo?.invoice_no, // Invoice Number
-    EBELN: btnInfo?.purchasing_doc_no, // PO Number
+    AENAM: result_qq[0].vendor_name, // Vendor Name
+    LIFNR: result_qq[0].vendor_code, // Vendor Codebtn_v2
+    ZVBNO: btnInfo[0]?.invoice_no, // Invoice Number
+    EBELN: btnInfo[0]?.purchasing_doc_no, // PO Number
     DPERNR1: "", // Not Required
     ZRMK1: "Forwared To Finance", // REMARKS
   };
@@ -583,7 +597,7 @@ console.log(result);
       res,
       false,
       200,
-      "Data not inserted! Try Again!",
+     JSON.stringify(result),
       null,
       null
     );
@@ -618,10 +632,10 @@ const getGrnIcrenPenelty = async (req, res) => {
         null
       );
     }
-
+    
 
     const gate_entry_q = `SELECT ENTRY_NO AS gate_entry_no,
-    ZMBLNR AS grn_no,
+    ZMBLNR AS grn_no, EBELP as po_lineitem,
     INV_DATE AS invoice_date FROM zmm_gate_entry_d WHERE EBELN = ? AND INVNO = ?`;
 
     let gate_entry_v = await query({
@@ -642,35 +656,43 @@ const getGrnIcrenPenelty = async (req, res) => {
       return resSend(res, true, 200, "No record found!", null, null);
     }
     gate_entry_v = gate_entry_v[0];
- 
-    const icgrn_q = `SELECT PRUEFLOS AS icgrn_nos, MATNR as mat_no, LMENGE01 as quantity FROM qals WHERE MBLNR = ?`; //   MBLNR (GRN No) PRUEFLOS (Lot Number)
+    console.log("gate_entry_v",gate_entry_v)
+
+    const icgrn_q = `SELECT PRUEFLOS AS icgrn_nos, MATNR as mat_no, LMENGE01 as quantity 
+    FROM qals WHERE MBLNR = ?`; //   MBLNR (GRN No) PRUEFLOS (Lot Number)
     let icgrn_no = await query({
       query: icgrn_q,
       values: [gate_entry_v?.grn_no],
     });
+
+    console.log("icgrn_no", icgrn_no)
  
     let total_price = 0;
+    let total_quantity = 0;
  
+    
     await Promise.all(
       await icgrn_no.map(async (item) => {
-        const price_q = `SELECT NETPR AS price FROM ekpo WHERE MATNR = ?`;
+        const price_q = `SELECT NETPR AS price FROM ekpo WHERE MATNR = ? and EBELN = ? and EBELP = ?`;
         let unit_price = await query({
           query: price_q,
-          values: [item?.mat_no],
+          values: [item?.mat_no, purchasing_doc_no, gate_entry_v.po_lineitem],
         });
-        console.log("unit_price", unit_price);
+        total_quantity += parseFloat(item?.quantity);
+        console.log("unit_price", unit_price, parseFloat(icgrn_no));
         await Promise.all(
           await unit_price.map(async (it) => {
             console.log("it_price", it.price, parseFloat(it?.price));
-            total_price += parseFloat(it?.price);
+            total_price += parseFloat(it?.price) * total_quantity;
           })
         );
       })
     );
-    gate_entry_v.total_price = total_price;
+    gate_entry_v.total_price = parseFloat(total_price.toFixed(2));
     gate_entry_v.icgrn_nos = gate_entry_v.grn_no;
     gate_entry_v.grn_nos = gate_entry_v.grn_no;
  
+    console.log
     return resSend(res, true, 200, "Data gate!", gate_entry_v, null);
   } catch (error) {
     console.error("Error making the request:", error.message);
@@ -678,6 +700,15 @@ const getGrnIcrenPenelty = async (req, res) => {
   }
 };
 
+const timeInHHMMSS = () => {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+
+  return hours+minutes+seconds;
+
+}
 
 module.exports = {
   fetchAllBTNs,
