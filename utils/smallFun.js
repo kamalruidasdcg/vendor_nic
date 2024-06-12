308d329d2ac706edee162e1dd26133d15b01f2d5
kamalruidasdcg <145760654+kamalruidasdcg@users.noreply.github.com>
2024-06-12 16:54:46 +0530

    Mrinmoy (#190)
    * 06062024
    
    * Sync File and Data
    
    ---------
    
    Co-authored-by: mrinmoygh081 <mrinmoygh081@gmail.com>

--- utils/smallFun.js
+++ utils/smallFun.js
@@ -1,3 +1,14 @@
 exports.checkTypeArr = (data) => {
   return data && Array.isArray(data) && data.length > 0;
 };
+
+exports.generateUnique = () => {
+  return new Date().getTime();
+};
+
+const path = require("path");
+
+exports.isZipFile = (filePath) => {
+  const ext = path.extname(filePath);
+  return ext === ".zip";
+};
