2f4b3e3d5a254b3d843506b0146e10e3ef087c5b
mainakweb <mainak.dutta16@gmail.com>
2024-06-15 19:49:24 +0530

    update drawing and ILMS

--- controllers/poController/ilmsController.js
+++ controllers/poController/ilmsController.js
@@ -4,7 +4,7 @@ const { handleFileDeletion } = require("../../lib/deleteFile");
 const { resSend } = require("../../lib/resSend");
 const { query, getQuery, poolClient, poolQuery } = require("../../config/pgDbConfig");
 const { generateQuery, getEpochTime } = require("../../lib/utils");
-const { INSERT, USER_TYPE_VENDOR, } = require("../../lib/constant");
+const { INSERT, USER_TYPE_VENDOR, USER_TYPE_GRSE_DRAWING, } = require("../../lib/constant");
 
 const { EKKO, NEW_SDBG, SDBG_ENTRY, SDBG, ILMS, DRAWING } = require("../../lib/tableName");
 const { SUBMITTED, ACCEPTED, APPROVED, REJECTED, ACKNOWLEDGED, } = require("../../lib/status");
@@ -13,12 +13,11 @@ const { getFilteredData } = require("../genralControlles");
 const SENDMAIL = require("../../lib/mailSend");
 const { SDBG_SUBMIT_MAIL_TEMPLATE } = require("../../templates/mail-template");
 const { mailInsert } = require("../../services/mail.services");
+const { checkIsApprovedRejected, getFristRow } = require("../../services/lastassignee.servces");
 // const { mailTrigger } = require("../sendMailController");
 
 
-// add new post
 const submitILMS = async (req, res) => {
-    // return resSend(res, false, 200, "No data 22inserted", req.body, null);
     try {
 
         const client = await poolClient();
@@ -37,29 +36,26 @@ const submitILMS = async (req, res) => {
 
             let payload = { ...req.body, ...fileData, created_at: getEpochTime() };
 
+            if (tokenData.user_type != USER_TYPE_VENDOR && tokenData.department_id != USER_TYPE_GRSE_DRAWING) {
+                return resSend(res, true, 200, "please login as Valid user!", null, null);
+            }
+
             if (!payload.purchasing_doc_no || !payload.type) {
                 return resSend(res, false, 200, "Please send valid payload", null, null);
             }
 
-            // 2 for drawind depertment//
-            if (tokenData.department_id == 2) {
+            if (tokenData.department_id == USER_TYPE_GRSE_DRAWING) {
                 if (!payload.reference_no || payload.reference_no == "") {
                     return resSend(res, false, 200, "Please send valid reference_no", null, null);
                 }
-
-                const GET_LATEST_ILMS = await get_latest_activity(ILMS, payload.purchasing_doc_no, payload.reference_no);
-
-                if (GET_LATEST_ILMS.status === APPROVED || GET_LATEST_ILMS.status === ACCEPTED || GET_LATEST_ILMS.status === ACKNOWLEDGED) {
-                    return resSend(res, false, 200, `this ILMS already ${GET_LATEST_ILMS.status}`, null, null);
+                const star = `vendor_code`;
+                const GET_FRIST_ILMS = await getFristRow(ILMS, star, payload.purchasing_doc_no);
+                const check = await checkIsApprovedRejected(ILMS, payload.purchasing_doc_no, payload.reference_no, APPROVED, REJECTED);
+                if (check > 0) {
+                    return resSend(res, false, 200, `You can't take any action against this reference_no.`, null, null);
                 }
-                // console.log(GET_LATEST_ILMS);
-                // return;
-                payload.file_name = GET_LATEST_ILMS.file_name;
-                payload.file_path = GET_LATEST_ILMS.file_path;
-                payload.vendor_code = GET_LATEST_ILMS.vendor_code;
+                payload.vendor_code = GET_FRIST_ILMS.vendor_code;
             }
-
-
 
             if (tokenData.user_type == USER_TYPE_VENDOR) {
                 payload.reference_no = await create_reference_no("ILMS", tokenData.vendor_code);
@@ -71,76 +67,69 @@ const submitILMS = async (req, res) => {
             payload.vendor_code = (tokenData.user_type === USER_TYPE_VENDOR) ? tokenData.vendor_code : payload.vendor_code;
             payload.updated_by = (tokenData.user_type === USER_TYPE_VENDOR) ? "VENDOR" : "GRSE";
             payload.created_by_id = tokenData.vendor_code;
-            // console.log("payload..");
-            // console.log(payload);
-            // return;
-            // const verifyStatus = [PENDING, RE_SUBMITTED];
-
-
-
-
+            
             const { q, val } = generateQuery(INSERT, ILMS, payload);
             console.log(q);
             console.log(val);
 
-            if (payload.status === APPROVED || payload.status === ACCEPTED || payload.status === ACKNOWLEDGED) {
+            if (payload.status === APPROVED || payload.status === REJECTED) {
                 const actual_subminission = await setActualSubmissionDate(payload, "04", tokenData, SUBMITTED);
                 console.log("actual_subminission", actual_subminission);
             }
             const response = await poolQuery({ client, query: q, values: val });
             console.log(response);
             console.log('#$%^&*&^%$#$%^&');
-           
-                // mail setup
 
-                // if (payload.status === PENDING) {
+            // mail setup
 
-                //     if (payload.updated_by == "VENDOR") {
+            // if (payload.status === PENDING) {
 
-                //         const result = await poContactDetails(payload.purchasing_doc_no);
-                //         payload.delingOfficerName = result[0]?.dealingOfficerName;
-                //         payload.mailSendTo = result[0]?.dealingOfficerMail;
-                //         payload.vendor_name = result[0]?.vendor_name;
-                //         payload.vendor_code = result[0]?.vendor_code;
-                //         payload.sendAt = new Date(payload.created_at);
-                //         mailTrigger({ ...payload }, SDBG_SUBMIT_BY_VENDOR);
+            //     if (payload.updated_by == "VENDOR") {
 
-                //     } else if (payload.updated_by == "GRSE") {
+            //         const result = await poContactDetails(payload.purchasing_doc_no);
+            //         payload.delingOfficerName = result[0]?.dealingOfficerName;
+            //         payload.mailSendTo = result[0]?.dealingOfficerMail;
+            //         payload.vendor_name = result[0]?.vendor_name;
+            //         payload.vendor_code = result[0]?.vendor_code;
+            //         payload.sendAt = new Date(payload.created_at);
+            //         mailTrigger({ ...payload }, SDBG_SUBMIT_BY_VENDOR);
 
-                //         const result = await poContactDetails(payload.purchasing_doc_no);
-                //         payload.vendor_name = result[0]?.vendor_name;
-                //         payload.vendor_code = result[0]?.vendor_code;
-                //         payload.mailSendTo = result[0]?.vendor_mail_id;
-                //         payload.delingOfficerName = result[0]?.dealingOfficerName;
-                //         payload.sendAt = new Date(payload.created_at);
+            //     } else if (payload.updated_by == "GRSE") {
 
-                //         mailTrigger({ ...payload }, SDBG_SUBMIT_BY_GRSE);
+            //         const result = await poContactDetails(payload.purchasing_doc_no);
+            //         payload.vendor_name = result[0]?.vendor_name;
+            //         payload.vendor_code = result[0]?.vendor_code;
+            //         payload.mailSendTo = result[0]?.vendor_mail_id;
+            //         payload.delingOfficerName = result[0]?.dealingOfficerName;
+            //         payload.sendAt = new Date(payload.created_at);
 
-                //     }
-                // }
-                // if (payload.status === ACKNOWLEDGED && payload.updated_by == "GRSE") {
+            //         mailTrigger({ ...payload }, SDBG_SUBMIT_BY_GRSE);
 
-                //     const result = await poContactDetails(payload.purchasing_doc_no);
-                //     payload.vendor_name = result[0]?.vendor_name;
-                //     payload.vendor_code = result[0]?.vendor_code;
-                //     payload.mailSendTo = result[0]?.vendor_mail_id;
-                //     payload.delingOfficerName = result[0]?.dealingOfficerName;
-                //     payload.sendAt = new Date(payload.created_at);
-                //     mailTrigger({ ...payload }, SDBG_SUBMIT_BY_GRSE);
+            //     }
+            // }
+            // if (payload.status === ACKNOWLEDGED && payload.updated_by == "GRSE") {
 
-                // }
+            //     const result = await poContactDetails(payload.purchasing_doc_no);
+            //     payload.vendor_name = result[0]?.vendor_name;
+            //     payload.vendor_code = result[0]?.vendor_code;
+            //     payload.mailSendTo = result[0]?.vendor_mail_id;
+            //     payload.delingOfficerName = result[0]?.dealingOfficerName;
+            //     payload.sendAt = new Date(payload.created_at);
+            //     mailTrigger({ ...payload }, SDBG_SUBMIT_BY_GRSE);
 
-                // await handelEmail(payload);
+            // }
 
-                 resSend(res, true, 200, `ILMS ${payload.status}!`, fileData, null);
-            
+            // await handelEmail(payload);
+
+            resSend(res, true, 200, `ILMS ${payload.status}!`, fileData, null);
+
         } catch (error) {
             console.log("ILMS Submission api", error.toString());
 
             return resSend(res, false, 500, "internal server error", [], null);
         } finally {
             client.release();
-          }
+        }
     } catch (error) {
 
         resSend(res, false, 500, "error in db conn!", error, "");
