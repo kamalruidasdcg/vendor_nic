2b375e42ede4ecb3953aa79103ba9cbf4997ba06
kamalruidasdcg <145760654+kamalruidasdcg@users.noreply.github.com>
2024-06-18 17:04:22 +0530

    Mrinmoy (#209)
    * 06062024
    
    * Sync File and Data
    
    * 1706 by upu
    
    * 18062024Mrin
    
    * 180620224BB
    
    * smallFixing
    
    ---------
    
    Co-authored-by: mrinmoygh081 <mrinmoygh081@gmail.com>

--- controllers/btnControllers.js
+++ controllers/btnControllers.js
@@ -14,9 +14,19 @@ const {
   USER_TYPE_VENDOR,
   UPDATE,
 } = require("../lib/constant");
-const { BTN_RETURN_DO, BTN_FORWORD_FINANCE, BTN_UPLOAD_CHECKLIST } = require("../lib/event");
+const {
+  BTN_RETURN_DO,
+  BTN_FORWORD_FINANCE,
+  BTN_UPLOAD_CHECKLIST,
+} = require("../lib/event");
 const { resSend } = require("../lib/resSend");
-const { APPROVED, SUBMITTED, FORWARD_TO_FINANCE, REJECTED, ASSIGNED } = require("../lib/status");
+const {
+  APPROVED,
+  SUBMITTED,
+  FORWARD_TO_FINANCE,
+  REJECTED,
+  ASSIGNED,
+} = require("../lib/status");
 const {
   BTN_MATERIAL,
   BTN_LIST,
@@ -377,17 +387,17 @@ const submitBTN = async (req, res) => {
 
   payloadFiles["e_invoice_filename"]
     ? (payload = {
-      ...payload,
-      e_invoice_filename: payloadFiles["e_invoice_filename"][0]?.filename,
-    })
+        ...payload,
+        e_invoice_filename: payloadFiles["e_invoice_filename"][0]?.filename,
+      })
     : null;
 
   payloadFiles["debit_credit_filename"]
     ? (payload = {
-      ...payload,
-      debit_credit_filename:
-        payloadFiles["debit_credit_filename"][0]?.filename,
-    })
+        ...payload,
+        debit_credit_filename:
+          payloadFiles["debit_credit_filename"][0]?.filename,
+      })
     : null;
 
   // GET Approved SDBG by PO Number
@@ -417,17 +427,17 @@ const submitBTN = async (req, res) => {
 
   payloadFiles["get_entry_filename"]
     ? (payload = {
-      ...payload,
-      get_entry_filename: payloadFiles["get_entry_filename"][0]?.filename,
-    })
+        ...payload,
+        get_entry_filename: payloadFiles["get_entry_filename"][0]?.filename,
+      })
     : null;
 
   payloadFiles["demand_raise_filename"]
     ? (payload = {
-      ...payload,
-      demand_raise_filename:
-        payloadFiles["demand_raise_filename"][0]?.filename,
-    })
+        ...payload,
+        demand_raise_filename:
+          payloadFiles["demand_raise_filename"][0]?.filename,
+      })
     : null;
 
   // generate btn num
@@ -858,59 +868,91 @@ const timeInHHMMSS = () => {
   return hours + minutes + seconds;
 };
 
-
 async function handelMail(tokenData, payload, event) {
-
-
   try {
-
     let emailUserDetailsQuery;
     let emailUserDetails;
     let dataObj = payload;
 
-
-    if (tokenData.user_type == USER_TYPE_VENDOR && payload.status == SUBMITTED) {
-
-      emailUserDetailsQuery = getUserDetailsQuery('vendor_and_do');
-      emailUserDetails = await getQuery({ query: emailUserDetailsQuery, values: [payload.purchasing_doc_no] });
-      console.log("emailUserDetailsQuery", emailUserDetailsQuery, emailUserDetails);
-      await sendMail(BTN_UPLOAD_CHECKLIST, dataObj, { users: emailUserDetails }, BTN_UPLOAD_CHECKLIST);
+    if (
+      tokenData.user_type == USER_TYPE_VENDOR &&
+      payload.status == SUBMITTED
+    ) {
+      emailUserDetailsQuery = getUserDetailsQuery("vendor_and_do");
+      emailUserDetails = await getQuery({
+        query: emailUserDetailsQuery,
+        values: [payload.purchasing_doc_no],
+      });
+      console.log(
+        "emailUserDetailsQuery",
+        emailUserDetailsQuery,
+        emailUserDetails
+      );
+      await sendMail(
+        BTN_UPLOAD_CHECKLIST,
+        dataObj,
+        { users: emailUserDetails },
+        BTN_UPLOAD_CHECKLIST
+      );
     }
 
-    if (tokenData.user_type != USER_TYPE_VENDOR && payload.status == FORWARD_TO_FINANCE) {
-      emailUserDetailsQuery = getUserDetailsQuery('venode_by_po');
-      emailUserDetails = await getQuery({ query: emailUserDetailsQuery, values: [payload.purchasing_doc_no] });
-      await sendMail(BTN_FORWORD_FINANCE, dataObj, { users: emailUserDetails }, BTN_FORWORD_FINANCE);
+    if (
+      tokenData.user_type != USER_TYPE_VENDOR &&
+      payload.status == FORWARD_TO_FINANCE
+    ) {
+      emailUserDetailsQuery = getUserDetailsQuery("venode_by_po");
+      emailUserDetails = await getQuery({
+        query: emailUserDetailsQuery,
+        values: [payload.purchasing_doc_no],
+      });
+      await sendMail(
+        BTN_FORWORD_FINANCE,
+        dataObj,
+        { users: emailUserDetails },
+        BTN_FORWORD_FINANCE
+      );
     }
-
 
     if (tokenData.user_type != USER_TYPE_VENDOR && payload.status == REJECTED) {
       // emailUserDetailsQuery = getUserDetailsQuery('vendor_by_po', '$1');
-      emailUserDetailsQuery =  getUserDetailsQuery('vendor_by_po', '$1');
+      emailUserDetailsQuery = getUserDetailsQuery("vendor_by_po", "$1");
 
-
-      emailUserDetails = await getQuery({ query: emailUserDetailsQuery, values: [payload.purchasing_doc_no] });
+      emailUserDetails = await getQuery({
+        query: emailUserDetailsQuery,
+        values: [payload.purchasing_doc_no],
+      });
       dataObj = { ...dataObj, vendor_name: emailUserDetails[0].u_name };
-      await sendMail(BTN_RETURN_DO, dataObj, { users: emailUserDetails }, BTN_RETURN_DO);
+      await sendMail(
+        BTN_RETURN_DO,
+        dataObj,
+        { users: emailUserDetails },
+        BTN_RETURN_DO
+      );
     }
     if (tokenData.user_type != USER_TYPE_VENDOR && payload.status == ASSIGNED) {
       // emailUserDetailsQuery = getUserDetailsQuery('vendor_by_po', '$1');
-      emailUserDetailsQuery = 'SELECT * FROM ('
-      buildQuery +=  getUserDetailsQuery('vendor_by_po', '$1');
-      buildQuery += 'UNION';
-      buildQuery += getUserDetailsQuery('assingee', '$2');
-      buildQuery += ') AS mail_info';
+      emailUserDetailsQuery = "SELECT * FROM (";
+      buildQuery += getUserDetailsQuery("vendor_by_po", "$1");
+      buildQuery += "UNION";
+      buildQuery += getUserDetailsQuery("assingee", "$2");
+      buildQuery += ") AS mail_info";
 
-      emailUserDetails = await getQuery({ query: emailUserDetailsQuery, values: [payload.purchasing_doc_no] });
+      emailUserDetails = await getQuery({
+        query: emailUserDetailsQuery,
+        values: [payload.purchasing_doc_no],
+      });
       dataObj = { ...dataObj, vendor_name: emailUserDetails[0].u_name };
-      await sendMail(BTN_RETURN_DO, dataObj, { users: emailUserDetails }, BTN_RETURN_DO);
+      await sendMail(
+        BTN_RETURN_DO,
+        dataObj,
+        { users: emailUserDetails },
+        BTN_RETURN_DO
+      );
     }
-
   } catch (error) {
     console.log("handelMail qap", error.toString(), error.stack);
   }
 }
-
 
 const assignToFiStaffHandler = async (req, res) => {
   const { btn_num, purchasing_doc_no, assign_to_fi } = req.body;
@@ -927,10 +969,10 @@ const assignToFiStaffHandler = async (req, res) => {
     return resSend(res, false, 200, "Assign To is the mandatory!", null, null);
   }
 
-  const assign_q = `SELECT * FROM ${BTN_ASSIGN} WHERE btn_num = $1 and last_assign = '1'`;
+  const assign_q = `SELECT * FROM ${BTN_ASSIGN} WHERE btn_num = $1 and last_assign = $2`;
   let assign_fi_staff_v = await getQuery({
     query: assign_q,
-    values: [btn_num],
+    values: [btn_num, true],
   });
   if (!checkTypeArr(assign_fi_staff_v)) {
     return resSend(
@@ -949,13 +991,13 @@ const assignToFiStaffHandler = async (req, res) => {
   const payload = {
     assign_by_fi: tokenData?.vendor_code,
     assign_to_fi: assign_to_fi,
+    last_assign_fi: true,
   };
 
   try {
     let { q, val } = generateQuery(UPDATE, BTN_ASSIGN, payload, whereCon);
-    console.log(q, val);
-
     let resp = await getQuery({ query: q, values: val });
+    console.log("resp", resp);
 
     let btn_list_q = `SELECT * FROM btn_list WHERE purchasing_doc_no = $1`;
     let btn_list = await getQuery({
@@ -991,7 +1033,7 @@ const assignToFiStaffHandler = async (req, res) => {
       resSend(res, false, 200, "Something went wrong in BTN List", null, null);
     }
   } catch (err) {
-    console.log("HELLO", err.message);
+    console.log("ERROR", err.message);
   }
 };
 
