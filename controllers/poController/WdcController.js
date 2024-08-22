0cf04a42b8f909e2ed010019de72d6aa29c4ce84
mainakweb <mainak.dutta16@gmail.com>
2024-08-22 12:54:11 +0530

    update in WdcController

--- controllers/poController/WdcController.js
+++ controllers/poController/WdcController.js
@@ -1,5 +1,10 @@
 const { resSend } = require("../../lib/resSend");
-const { query, getQuery, poolClient, poolQuery } = require("../../config/pgDbConfig");
+const {
+  query,
+  getQuery,
+  poolClient,
+  poolQuery,
+} = require("../../config/pgDbConfig");
 const { generateQuery, getEpochTime } = require("../../lib/utils");
 const {
   INSERT,
@@ -29,7 +34,9 @@ const { makeHttpRequest } = require("../../config/sapServerConfig");
 const { getUserDetailsQuery } = require("../../utils/mailFunc");
 const { sendMail } = require("../../services/mail.services");
 const { WDC_APPROVAL_REJECT, WDC_UPLOADING } = require("../../lib/event");
-const { checkIsApprovedRejected } = require("../../services/lastassignee.servces");
+const {
+  checkIsApprovedRejected,
+} = require("../../services/lastassignee.servces");
 require("dotenv").config();
 
 exports.wdc = async (req, res) => {
@@ -46,12 +53,10 @@ exports.wdc = async (req, res) => {
     }
 
     //  deper
-    if (
-      tokenData.user_type != USER_TYPE_VENDOR &&
-      tokenData.department_id != USER_TYPE_PPNC_DEPARTMENT
-    ) {
-      return resSend(res, false, 200, "You are not authorised!", null, null);
+    if (obj.status == SUBMITTED && tokenData.user_type != USER_TYPE_VENDOR) {
+      return resSend(res, false, 200, "Vendor only can submit", null, null);
     }
+
     //console.log(obj);
     if (tokenData.user_type == USER_TYPE_VENDOR) {
       if (!obj.action_type || obj.action_type == "") {
@@ -62,7 +67,6 @@ exports.wdc = async (req, res) => {
         return resSend(res, false, 200, "VENDOR ONLY CAN SUBMIT!", null, null);
       }
     }
-
 
     //return;
 
@@ -89,19 +93,42 @@ exports.wdc = async (req, res) => {
         );
       }
 
-      const check = await checkIsApprovedRejected(WDC, obj.purchasing_doc_no, obj.reference_no, APPROVED, REJECTED);
+      const check = await checkIsApprovedRejected(
+        WDC,
+        obj.purchasing_doc_no,
+        obj.reference_no,
+        APPROVED,
+        REJECTED
+      );
       if (check > 0) {
-          return resSend(res, false, 200, `You can't take any action against this reference no.`, null, null);
+        return resSend(
+          res,
+          false,
+          200,
+          `You can't take any action against this reference no.`,
+          null,
+          null
+        );
       }
 
       const line_item_array_q = `SELECT COUNT(assigned_to) AS assingn from ${WDC} WHERE purchasing_doc_no = $1 AND  assigned_to = $2`;
-      let line_item_array = await getQuery({ query: line_item_array_q, values: [obj.purchasing_doc_no, tokenData.vendor_code] });
+      let line_item_array = await getQuery({
+        query: line_item_array_q,
+        values: [obj.purchasing_doc_no, tokenData.vendor_code],
+      });
       //  console.log("#$%^&*(*&^%$#");
       //  console.log(line_item_array);
       //  console.log("$%^&*()(*&^&*");
       //  return;
       if (line_item_array[0].assingn == 0) {
-        return resSend(res, false, 200, "You are not authorised person!", null, null);
+        return resSend(
+          res,
+          false,
+          200,
+          "You are not authorised person!",
+          null,
+          null
+        );
       }
       last_data = await get_latest_activity(
         WDC,
@@ -157,7 +184,6 @@ exports.wdc = async (req, res) => {
           created_by_name: tokenData.iss,
           created_at: getEpochTime(),
         };
-
       } else {
         return resSend(
           res,
@@ -178,32 +204,41 @@ exports.wdc = async (req, res) => {
       );
 
       // Handle uploaded files
-      obj.file_inspection_note_ref_no =
-        payloadFiles["file_inspection_note_ref_no"]
-          ? (invoice_filename = payloadFiles["file_inspection_note_ref_no"][0]?.filename)
-          : null;
+      obj.file_inspection_note_ref_no = payloadFiles[
+        "file_inspection_note_ref_no"
+      ]
+        ? (invoice_filename =
+            payloadFiles["file_inspection_note_ref_no"][0]?.filename)
+        : null;
 
-      obj.file_hinderence_report_cerified_by_berth =
-        payloadFiles["file_hinderence_report_cerified_by_berth"]
-          ? (invoice_filename = payloadFiles["file_hinderence_report_cerified_by_berth"][0]?.filename)
-          : null;
+      obj.file_hinderence_report_cerified_by_berth = payloadFiles[
+        "file_hinderence_report_cerified_by_berth"
+      ]
+        ? (invoice_filename =
+            payloadFiles["file_hinderence_report_cerified_by_berth"][0]
+              ?.filename)
+        : null;
 
-      obj.file_attendance_report =
-        payloadFiles["file_attendance_report"]
-          ? (invoice_filename = payloadFiles["file_attendance_report"][0]?.filename)
-          : null;
+      obj.file_attendance_report = payloadFiles["file_attendance_report"]
+        ? (invoice_filename =
+            payloadFiles["file_attendance_report"][0]?.filename)
+        : null;
 
       // obj.line_item_array = '[{"claim_qty":"4","line_item_no":"15","actual_start_date":"2024-05-07T18:30:00.000Z","actual_completion_date":"2024-05-09T18:30:00.000Z","delay_in_work_execution":"1"},{"claim_qty":"6","line_item_no":"40","actual_start_date":"2024-05-20T18:30:00.000Z","actual_completion_date":"2024-05-22T18:30:00.000Z","delay_in_work_execution":"2"}]';
       let line_item_array = JSON.parse(obj.line_item_array);
       obj.total_amount = 0;
 
-      if (obj.action_type == 'JCC' && obj.line_item_array != "") {
-
+      if (obj.action_type == "JCC" && obj.line_item_array != "") {
         const line_item_array_q = `SELECT EBELP AS line_item_no, NETPR AS po_rate from ${EKPO} WHERE EBELN = $1`;
-        let get_data_result = await getQuery({ query: line_item_array_q, values: [obj.purchasing_doc_no] });
+        let get_data_result = await getQuery({
+          query: line_item_array_q,
+          values: [obj.purchasing_doc_no],
+        });
 
         obj.line_item_array = line_item_array.map((el2) => {
-          const DOObj = get_data_result.find((elms) => elms.line_item_no == el2.line_item_no);
+          const DOObj = get_data_result.find(
+            (elms) => elms.line_item_no == el2.line_item_no
+          );
           let total = parseFloat(DOObj.po_rate) * parseFloat(el2.claim_qty);
 
           total = total.toFixed(2);
@@ -212,9 +247,7 @@ exports.wdc = async (req, res) => {
         });
 
         obj.line_item_array = JSON.stringify(obj.line_item_array);
-
       }
-
 
       payload = {
         ...fileData,
@@ -228,15 +261,12 @@ exports.wdc = async (req, res) => {
       };
     }
 
-
     const { q, val } = generateQuery(INSERT, WDC, payload);
     let response = await getQuery({ query: q, values: val });
     response = response[0];
 
     if (response) {
-
-      handelMail(tokenData, payload)
-
+      handelMail(tokenData, payload);
 
       if (payload.status === SUBMITTED) {
         // handel email
@@ -252,12 +282,10 @@ exports.wdc = async (req, res) => {
           console.log(payload);
           console.log("payload$%^&*(");
           const submit = await submitToSapServer(payload);
-            //await client.query('COMMIT');
-            if(submit == false) {
-              //await client.query('ROLLBACK');
-            }
-
-
+          //await client.query('COMMIT');
+          if (submit == false) {
+            //await client.query('ROLLBACK');
+          }
         } catch (error) {
           console.warn(
             "WDC/JCC save in sap faild, please refer to wdcContorller submitToSapServer fn"
@@ -282,44 +310,62 @@ exports.wdc = async (req, res) => {
   }
 };
 
-
-
-
 async function handelMail(tokenData, payload, event) {
-
-
   try {
-
     let emailUserDetailsQuery;
     let emailUserDetails;
     let dataObj = payload;
 
-
-    if ( payload.status == SUBMITTED) {
+    if (payload.status == SUBMITTED) {
       // QA NODAL OFFICERS
-      emailUserDetailsQuery = getUserDetailsQuery('wdc_certifing_authrity',' $1');
-      emailUserDetails = await getQuery({ query: emailUserDetailsQuery, values: [parseInt(payload.assigned_to)] });
-      await sendMail(WDC_UPLOADING, dataObj, { users: emailUserDetails }, WDC_UPLOADING);
+      emailUserDetailsQuery = getUserDetailsQuery(
+        "wdc_certifing_authrity",
+        " $1"
+      );
+      emailUserDetails = await getQuery({
+        query: emailUserDetailsQuery,
+        values: [parseInt(payload.assigned_to)],
+      });
+      await sendMail(
+        WDC_UPLOADING,
+        dataObj,
+        { users: emailUserDetails },
+        WDC_UPLOADING
+      );
     }
 
-    if ( payload.status == REJECTED) {
+    if (payload.status == REJECTED) {
       // QA NODAL OFFICERS
-      emailUserDetailsQuery = getUserDetailsQuery('vendor_by_po', '$1');
-      emailUserDetails = await getQuery({ query: emailUserDetailsQuery, values: [payload.purchasing_doc_no] });
-      await sendMail(WDC_APPROVAL_REJECT, dataObj, { users: emailUserDetails }, WDC_APPROVAL_REJECT);
+      emailUserDetailsQuery = getUserDetailsQuery("vendor_by_po", "$1");
+      emailUserDetails = await getQuery({
+        query: emailUserDetailsQuery,
+        values: [payload.purchasing_doc_no],
+      });
+      await sendMail(
+        WDC_APPROVAL_REJECT,
+        dataObj,
+        { users: emailUserDetails },
+        WDC_APPROVAL_REJECT
+      );
     }
     if (payload.status == APPROVED) {
       // QA NODAL OFFICERS
-      emailUserDetailsQuery = getUserDetailsQuery('vendor_by_po', '$1');
-      emailUserDetails = await getQuery({ query: emailUserDetailsQuery, values: [payload.purchasing_doc_no] });
-      await sendMail(WDC_APPROVAL_REJECT, dataObj, { users: emailUserDetails }, WDC_APPROVAL_REJECT);
+      emailUserDetailsQuery = getUserDetailsQuery("vendor_by_po", "$1");
+      emailUserDetails = await getQuery({
+        query: emailUserDetailsQuery,
+        values: [payload.purchasing_doc_no],
+      });
+      await sendMail(
+        WDC_APPROVAL_REJECT,
+        dataObj,
+        { users: emailUserDetails },
+        WDC_APPROVAL_REJECT
+      );
     }
-
   } catch (error) {
     console.log("handelMail qap", error.toString(), error.stack);
   }
 }
-
 
 exports.list = async (req, res) => {
   // req.query.$tableName = `wdc`;
@@ -331,16 +377,18 @@ exports.list = async (req, res) => {
   // }
   //resSend(res, true, 200, "oded!", req.query.dd, null);
 
-
   // console.log(get_data_result);
   // return;
 
   try {
-
     const client = await poolClient();
     try {
       const line_item_array_q = `SELECT EBELP AS line_item_no, TXZ01 AS description, MATNR AS matarial_code, MEINS AS unit, MENGE AS target_amount from ${EKPO} WHERE EBELN = $1`;
-      let line_item_array = await poolQuery({ client, query: line_item_array_q, values: [req.query.poNo] });
+      let line_item_array = await poolQuery({
+        client,
+        query: line_item_array_q,
+        values: [req.query.poNo],
+      });
 
       //return;
       let line_item_array2 = [];
@@ -348,69 +396,87 @@ exports.list = async (req, res) => {
         line_item_array.map(async (els) => {
           //console.log(els);
           const total_amount_query = `SELECT SUM(MENGE) AS total_amount from mseg WHERE EBELN = $1 AND EBELP = $2`;
-          let total_amount_result = await poolQuery({ client, query: total_amount_query, values: [req.query.poNo, els.line_item_no] });
-          total_amount_result = (total_amount_result[0].total_amount == null) ? 0 : total_amount_result[0].total_amount;
+          let total_amount_result = await poolQuery({
+            client,
+            query: total_amount_query,
+            values: [req.query.poNo, els.line_item_no],
+          });
+          total_amount_result =
+            total_amount_result[0].total_amount == null
+              ? 0
+              : total_amount_result[0].total_amount;
           console.log(total_amount_result);
-          let rest_amount = parseInt(els.target_amount) - parseInt(total_amount_result);
-          (rest_amount < 0) ? 0 : rest_amount;
+          let rest_amount =
+            parseInt(els.target_amount) - parseInt(total_amount_result);
+          rest_amount < 0 ? 0 : rest_amount;
           line_item_array2.push({ ...els, rest_amount: rest_amount });
-
         })
-
       );
-
 
       //return;
       const get_data_query = `SELECT * FROM ${WDC} WHERE purchasing_doc_no = $1`;
-      let get_data_result = await poolQuery({ client, query: get_data_query, values: [req.query.poNo] });
+      let get_data_result = await poolQuery({
+        client,
+        query: get_data_query,
+        values: [req.query.poNo],
+      });
 
       let modfResult = get_data_result.map((el) => {
         let line_item = JSON.parse(el.line_item_array);
         let line_item2;
         if (line_item && Array.isArray(line_item)) {
           line_item2 = line_item.map((el2) => {
-            const DOObj = line_item_array2.find((elms) => elms.line_item_no == el2.line_item_no);
+            const DOObj = line_item_array2.find(
+              (elms) => elms.line_item_no == el2.line_item_no
+            );
             return DOObj ? { ...DOObj, ...el2 } : el2;
           });
         }
         el.line_item_array = line_item2;
         return el;
-      })
+      });
       modfResult = JSON.stringify(modfResult);
       resSend(res, true, 200, "WDC data fetched!", modfResult, null);
     } catch (err) {
-
       resSend(res, false, 500, "internal server error", err, null);
     } finally {
       client.release();
     }
-
   } catch (error) {
-
     resSend(res, false, 500, "error in db conn!", error, "");
   }
-
 };
 
 const getLineItemArray = async (poNo) => {
   const line_item_array_q = `SELECT EBELP AS line_item_no, TXZ01 AS description, MATNR AS matarial_code, MEINS AS unit, KTMNG AS target_amount from ${EKPO} WHERE EBELN = ?`;
-  let get_data_result = await query({ query: line_item_array_q, values: [poNo] });
+  let get_data_result = await query({
+    query: line_item_array_q,
+    values: [poNo],
+  });
 
   const total_amount_query = `SELECT SUM(MENGE) AS total_amount from mseg WHERE EBELN = ? AND EBELP = ?`;
-  let total_amount_result = await query({ query: total_amount_query, values: [req.query.po_no, req.query.line_item_no] });
-  total_amount_result = (total_amount_result[0].total_amount == null) ? 0 : total_amount_result[0].total_amount;
+  let total_amount_result = await query({
+    query: total_amount_query,
+    values: [req.query.po_no, req.query.line_item_no],
+  });
+  total_amount_result =
+    total_amount_result[0].total_amount == null
+      ? 0
+      : total_amount_result[0].total_amount;
   console.log("total_amount_result :" + total_amount_result);
 
-  let target_amount_result = (get_data_result[0].target_amount) ? get_data_result[0].target_amount : 0;
+  let target_amount_result = get_data_result[0].target_amount
+    ? get_data_result[0].target_amount
+    : 0;
 
-  const rest_amount = parseInt(target_amount_result) - parseInt(total_amount_result);
+  const rest_amount =
+    parseInt(target_amount_result) - parseInt(total_amount_result);
 
   // const resData = get_data_result[0];
   //           resData.rest_amount = rest_amount;
 
   return { ...get_data_result[0], rest_amount: rest_amount };
-
-}
+};
 
 exports.grseEmpList = async (req, res) => {
   req.query.$tableName = `pa0002`;
@@ -420,7 +486,6 @@ exports.grseEmpList = async (req, res) => {
   } catch (err) {
     console.log("data not fetched", err);
   }
-
 };
 
 async function submitToSapServer(data) {
@@ -442,16 +507,24 @@ async function submitToSapServer(data) {
     let wdc_payload;
     if (new_line_item && Array.isArray(new_line_item)) {
       wdc_payload = new_line_item.map((el2) => {
-        return { slno: payload.slno, ebeln: payload.purchasing_doc_no, ebelp: el2.line_item_no, wdc: payload.reference_no };
+        return {
+          slno: payload.slno,
+          ebeln: payload.purchasing_doc_no,
+          ebelp: el2.line_item_no,
+          wdc: payload.reference_no,
+        };
       });
     }
-
 
     console.log("wdc_payload-----------");
     console.log(wdc_payload);
 
     const postResponse = await makeHttpRequest(postUrl, "POST", wdc_payload);
-    if(postResponse.statusCode && postResponse.statusCode >= 200 && postResponse.statusCode <= 226) {
+    if (
+      postResponse.statusCode &&
+      postResponse.statusCode >= 200 &&
+      postResponse.statusCode <= 226
+    ) {
       status = true;
     }
     console.log("POST Response from the server:", postResponse);
@@ -522,8 +595,6 @@ async function submitToSapServer(data) {
 //       },
 //     ]
 
-
-
 // {
 //   "purchasing_doc_no": "",
 //   "reference_no": "",
@@ -573,8 +644,6 @@ async function submitToSapServer(data) {
 //   "action_type":"JCC"
 // }
 
-
-
 ///////////// JCC //////////////////
 //// GRSE SUBMIT //////////
 // {
@@ -595,4 +664,3 @@ async function submitToSapServer(data) {
 //   "total_amount_status": "APPROVED",
 //   "action_type":"JCC"
 // }
-
