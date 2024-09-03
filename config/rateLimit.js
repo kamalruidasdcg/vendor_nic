d11cb77da3cde6fa71c5b7847be6814e6aaf3470
unknown <kamal.ruidas@datacoresystems.co.in>
2024-09-03 12:28:27 +0530

    rate limiter middleware

--- config/rateLimit.js
+++ config/rateLimit.js
@@ -5,8 +5,8 @@ function createRateLimiter() {
   const LIMIT_TIME_IN_MS = 1 * 60 * 1000;
   const MAX_REQUEST = 100;
   return rateLimit({
-    windowMs: LIMIT_TIME_IN_MS, // 1 minutes window
-    max: MAX_REQUEST, // Limit each IP to 100 requests per windowMs
+    windowMs: LIMIT_TIME_IN_MS, // LIMIT_TIME_IN_MS(milisecond) minutes window
+    max: MAX_REQUEST, // Limit each IP to MAX_REQUEST requests per windowMs
     message: `Too many requests from this IP, please try again after  ${LIMIT_TIME_IN_MS / (60 * 1000)} minutes.`,
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
