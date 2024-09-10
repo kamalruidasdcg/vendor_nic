37a5c27ca61536801c72136b8d89e3171121e372
mainakweb <mainak.dutta16@gmail.com>
2024-09-10 14:08:51 +0530

    CREATE BG AND DRAWING API

--- controllers/statControllers.js
+++ controllers/statControllers.js
@@ -14,17 +14,25 @@ const {
   BTN_ASSIGN,
   BTN_MATERIAL_DO,
   QAP_SUBMISSION,
+  DRAWING,
 } = require("../lib/tableName");
 const { checkPoType } = require("../services/lastassignee.servces");
-const { USER_TYPE_GRSE_QAP, ASSIGNER, STAFF } = require("../lib/constant");
-const { ASSIGNED, ACCEPTED } = require("../lib/status");
+const {
+  USER_TYPE_GRSE_QAP,
+  ASSIGNER,
+  STAFF,
+  USER_TYPE_IT,
+  USER_TYPE_GRSE_FINANCE,
+  USER_TYPE_GRSE_DRAWING,
+} = require("../lib/constant");
+const { ASSIGNED, ACCEPTED, APPROVED } = require("../lib/status");
 
 exports.statsForBG = async (req, res) => {
   try {
     const client = await poolClient();
     try {
-      const { tokenData } = req;
-
+      const tokenData = { ...req.tokenData };
+      console.log(tokenData);
       if (tokenData) {
         const Q = `
         SELECT * 
@@ -40,9 +48,9 @@ exports.statsForBG = async (req, res) => {
         });
         // For grse_FINANCE_ASSIGNER
         if (
-          result[0].internal_role_id === 1 &&
-          result[0].department_id === 15 &&
-          result[0].department_id === 1
+          (tokenData.internal_role_id === ASSIGNER &&
+            tokenData.department_id === USER_TYPE_GRSE_FINANCE) ||
+          tokenData.department_id === USER_TYPE_IT
         ) {
           const Q = `SELECT se.* FROM ${SDBG_ENTRY} se ORDER BY se.created_at DESC`;
           const result = await poolQuery({
@@ -71,8 +79,8 @@ exports.statsForBG = async (req, res) => {
         }
         // For grse_FINANCE_STAFF
         if (
-          result[0].internal_role_id === 2 &&
-          result[0].department_id === 15
+          result[0].internal_role_id === STAFF &&
+          result[0].department_id === USER_TYPE_GRSE_FINANCE
         ) {
           const Q = ` 
           SELECT se.*
@@ -133,7 +141,7 @@ exports.statsForBTN = async (req, res) => {
   try {
     const client = await poolClient();
     try {
-      const tokenData = req.tokenData;
+      const tokenData = { ...req.tokenData };
       if (tokenData) {
         //const QbtnNo = `SELECT MAX(created_at) as time, btn_num FROM ${BTN_LIST} GROUP BY btn_num,purchasing_doc_no`;
         const QbtnNo = `SELECT DISTINCT ON (btn_num)
@@ -159,9 +167,9 @@ exports.statsForBTN = async (req, res) => {
 
         // For grse_FINANCE_ASSIGNER
         if (
-          tokenData.internal_role_id === 1 &&
-          tokenData.department_id === 15 &&
-          result[0].department_id === 1
+          (tokenData.internal_role_id === ASSIGNER &&
+            tokenData.department_id === USER_TYPE_GRSE_FINANCE) ||
+          tokenData.department_id === USER_TYPE_IT
         ) {
           // const Q = `SELECT * FROM ${BTN_LIST} WHERE created_at IN(${str}) ORDER BY created_at DESC`;
 
@@ -210,8 +218,8 @@ exports.statsForBTN = async (req, res) => {
         }
         // For grse_FINANCE_STAFF
         if (
-          tokenData.internal_role_id === 2 &&
-          tokenData.department_id === 15
+          tokenData.internal_role_id === STAFF &&
+          tokenData.department_id === USER_TYPE_GRSE_FINANCE
         ) {
           // const query = `SELECT * FROM ${BTN_LIST} bl
           //                JOIN ${BTN_MATERIAL_DO} bdo ON bl.btn_num = bdo.btn_num
@@ -297,12 +305,13 @@ exports.statsForQA = async (req, res) => {
   try {
     const client = await poolClient();
     try {
+      //req.query.reference_no
       const tokenData = { ...req.tokenData };
 
       // check user from QA
       if (
-        tokenData.department_id != USER_TYPE_GRSE_QAP ||
-        tokenData.department_id != 1
+        tokenData.department_id != USER_TYPE_GRSE_QAP &&
+        tokenData.department_id != USER_TYPE_IT
       ) {
         return resSend(
           res,
@@ -412,7 +421,7 @@ exports.statsForQA = async (req, res) => {
       // return;
       const getAsfromAstoQ = `SELECT DISTINCT ON (purchasing_doc_no) purchasing_doc_no,assigned_from,assigned_to 
               FROM qap_submission WHERE purchasing_doc_no IN(${str})
-                AND is_assign = 1`;
+                AND ${ASSIGNED} = 1`;
 
       const getAsfromAstoData = await poolQuery({
         client,
@@ -450,6 +459,209 @@ exports.statsForQA = async (req, res) => {
               ...el2,
               assigned_from: "N/A",
               assigned_to: "N/A",
+            };
+          }
+        });
+      }
+
+      return resSend(
+        res,
+        true,
+        200,
+        "Data fetched successfully.",
+        results,
+        null
+      );
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      return resSend(res, false, 400, "Data not fetched!", null, error);
+    }
+  } catch (error) {
+    console.error("Error in DB connection:", error);
+    resSend(res, false, 500, "Error in DB connection!", null, error);
+  }
+};
+
+exports.stats = async (req, res) => {
+  // console.log(req.query.type);
+  // return;
+  try {
+    const client = await poolClient();
+    try {
+      //req.query.reference_no
+      const tokenData = { ...req.tokenData };
+
+      // check type
+      const type = req.query.type;
+      //
+
+      if (!type || (type != QAP_SUBMISSION && type != DRAWING)) {
+        return resSend(res, false, 200, "please send a type.", null, null);
+      }
+
+      const TABLE = type;
+      const ENTITY = type === QAP_SUBMISSION ? "QAP" : "DRAWING";
+      const STATUS = type === QAP_SUBMISSION ? ACCEPTED : APPROVED;
+      const USER_DEPT_ID =
+        type === QAP_SUBMISSION ? USER_TYPE_GRSE_QAP : USER_TYPE_GRSE_DRAWING;
+      const ASSIGNED_FROM =
+        type === QAP_SUBMISSION ? "assigned_from" : "assign_from";
+      const ASSIGNED_TO = type === QAP_SUBMISSION ? "assigned_to" : "assign_to";
+      const ASSIGNED = type === QAP_SUBMISSION ? "is_assign" : "last_assigned";
+
+      if (
+        tokenData.department_id != USER_DEPT_ID &&
+        tokenData.department_id != USER_TYPE_IT
+      ) {
+        return resSend(
+          res,
+          false,
+          200,
+          "You are not authorized to view the information",
+          null,
+          null
+        );
+      }
+      // console.log(111111111111);
+      // return;
+
+      let str = "";
+      let q;
+      let result;
+      // For grse_QA_ASSIGNER
+      if (tokenData.internal_role_id === ASSIGNER) {
+        q = `SELECT DISTINCT ON (t1.reference_no)
+            t1.reference_no,
+            t1.purchasing_doc_no,
+            t1.vendor_code,
+            t1.status,
+            t1.created_at,
+            t2.name1 as vendor_name
+            FROM ${TABLE} AS t1
+                          LEFT JOIN
+                              lfa1 AS t2
+                          ON
+                              t1.vendor_code = t2.lifnr
+                        
+          WHERE t1.reference_no != '${ENTITY} ASSIGNED'
+            ORDER BY  t1.reference_no,t1.created_at DESC`;
+
+        result = await poolQuery({
+          client,
+          query: q,
+          values: [],
+        });
+
+        let strArr = [];
+        if (result.length) {
+          result.forEach((item) => {
+            strArr.push(item.purchasing_doc_no);
+          });
+        }
+
+        function removeDuplicates(strArr) {
+          return [...new Set(strArr)];
+        }
+        strArr = removeDuplicates(strArr);
+        console.log("************");
+
+        if (strArr.length) {
+          strArr.forEach((item) => {
+            const no = item.purchasing_doc_no;
+            str = str.concat("'").concat(`${item}`).concat("'").concat(",");
+          });
+        }
+
+        str = str.slice(0, -1);
+      }
+
+      // For grse_QA_STAFF
+      if (tokenData.internal_role_id === STAFF) {
+        const getStrArrQ = `SELECT DISTINCT ON (purchasing_doc_no) purchasing_doc_no,assigned_to 
+              FROM ${TABLE} WHERE assigned_to = $1`;
+
+        const getStrArrData = await poolQuery({
+          client,
+          query: getStrArrQ,
+          values: [tokenData.vendor_code],
+        });
+        console.log(getStrArrData);
+        // return;
+        if (getStrArrData.length) {
+          getStrArrData.forEach((item) => {
+            const no = item.purchasing_doc_no;
+            str = str.concat("'").concat(`${no}`).concat("'").concat(",");
+          });
+        }
+
+        str = str.slice(0, -1);
+
+        let q = `SELECT DISTINCT ON (t1.reference_no)
+            t1.reference_no,
+            t1.purchasing_doc_no,
+            t1.vendor_code,
+            t1.status,
+            t1.created_at,
+            t2.name1 as vendor_name
+            FROM ${TABLE} AS t1
+                          LEFT JOIN
+                              lfa1 AS t2
+                          ON
+                              t1.vendor_code = t2.lifnr
+                        
+          WHERE t1.purchasing_doc_no IN (${str}) AND t1.reference_no != '${ENTITY} ASSIGNED'
+            ORDER BY  t1.reference_no,t1.created_at DESC`;
+
+        result = await poolQuery({
+          client,
+          query: q,
+          values: [],
+        });
+      }
+      // console.log(str);
+      // console.log(result);
+      // return;
+      const getAsfromAstoQ = `SELECT DISTINCT ON (purchasing_doc_no) purchasing_doc_no,${ASSIGNED_FROM},${ASSIGNED_TO} 
+              FROM ${TABLE} WHERE purchasing_doc_no IN(${str})
+                AND ${ASSIGNED} = 1`;
+
+      const getAsfromAstoData = await poolQuery({
+        client,
+        query: getAsfromAstoQ,
+        values: [],
+      });
+
+      const getStatusAccepctedQ = `SELECT DISTINCT ON (status) purchasing_doc_no,created_at 
+        FROM ${TABLE} WHERE purchasing_doc_no IN(${str})
+          AND status = $1`;
+
+      const getStatusAccepctedData = await poolQuery({
+        client,
+        query: getStatusAccepctedQ,
+        values: [STATUS],
+      });
+
+      let results = [];
+      if (result && Array.isArray(result)) {
+        results = result.map((el2) => {
+          let staAcc = getStatusAccepctedData.find(
+            (ele) => ele.purchasing_doc_no == el2.purchasing_doc_no
+          );
+          staAcc
+            ? (el2.accepted_on = staAcc.created_at)
+            : (el2.accepted_on = "N/A");
+
+          let DOObj = getAsfromAstoData.find(
+            (elms) => elms.purchasing_doc_no == el2.purchasing_doc_no
+          );
+          if (DOObj) {
+            return { ...DOObj, ...el2, time_taken: "N/A" };
+          } else {
+            return {
+              ...el2,
+              assigned_from: "N/A",
+              assigned_to: "N/A",
+              time_taken: "N/A",
             };
           }
         });
